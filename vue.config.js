889f7057274ffd1892eebc6712287a4d50fba53b
hangtang <Lht283499667@163.com>
2025-01-27 23:36:35 +0800

    deployment in history mode

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  base: './'
+  publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'assets'
 })
